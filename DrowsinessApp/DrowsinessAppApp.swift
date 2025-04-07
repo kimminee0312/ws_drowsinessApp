97e12558270faee8fcef946c0cd28645cbfa0c14
YoungRok-SON <dudfhr3349@gmail.com>
2025-04-08 02:56:09 +0900

    Initial Commit

---
+++ DrowsinessApp/DrowsinessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrowsinessAppApp.swift
+//  DrowsinessApp
+//
+//  Created by YoungRok Son on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DrowsinessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrowsinessAppTests/DrowsinessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrowsinessAppTests.swift
+//  DrowsinessAppTests
+//
+//  Created by YoungRok Son on 4/8/25.
+//
+
+import Testing
+@testable import DrowsinessApp
+
+struct DrowsinessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
